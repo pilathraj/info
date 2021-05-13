fc895ebb2195720ea6c599e6d5707289ddd0eacf
pilathraj <pilathraj@gmail.com>
2021-05-13 14:37:45 +0530

    Update Program.cs

--- c#/HelloWorld/Program.cs
+++ c#/HelloWorld/Program.cs
@@ -7,9 +7,28 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.Write("Enter your name: "); 
+            Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine("Hello {0}!!", name);
+            int i = 10;
+            string s = "Pilathraj";
+            string s1 = null;
+            Console.WriteLine("{0}, {1}", i, s);
+            Console.WriteLine(s1??"s1");
+            s1 ??= "test";
+            Console.WriteLine(s1);
+
         }
     }
 }
+/*
+/d/cs/st/Overview/HelloWorld
+$ dotnet run
+Hello World!
+Enter your name: p
+Hello p!!    
+10, Pilathraj
+s1
+test
+*/
+
