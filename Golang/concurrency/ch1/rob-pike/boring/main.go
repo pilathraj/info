41c47888311a8dbcd05479be4153717993030d2b
pilathraj <pilathraj@gmail.com>
2021-06-13 13:06:36 +0530

    Create main.go

---
+++ Golang/concurrency/ch1/rob-pike/boring/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func boring(s string) <- chan string {
+ ch := make(chan string)
+ go func(){
+   for i:=0;;i++{
+    ch <- fmt.Sprintf("%s %d", s, i)
+   }
+ }()
+ return ch
+
+} 
+
+func main() {
+	fmt.Println("Hello, playground")
+	c := boring("boring")
+	fmt.Println(c, len(c))
+	for i:=0; i<5; i++{
+	  fmt.Println(<-c)
+	}
+	fmt.Println("Program exits")
+	
+}
