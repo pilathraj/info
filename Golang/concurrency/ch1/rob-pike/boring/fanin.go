046929148f6ef423f1e71b2d51ddca3d52e48f96
pilathraj <pilathraj@gmail.com>
2021-06-14 10:31:37 +0530

    Create fanin.go

---
+++ Golang/concurrency/ch1/rob-pike/boring/fanin.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func boring(msg string) <- chan string {
+  ch := make(chan string)
+  go func(){
+    for i :=0; ; i++{
+      ch <- fmt.Sprintf("%s %d", msg, i)
+    }
+  }()
+  return ch
+}
+
+func fanin(input1, input2 <-chan string) <-chan string{
+  c := make(chan string)
+  go func(){
+    c<-<-input1
+  }
+   go func(){
+    c<-<-input2
+  }
+  return c
+}
+
+func main(){
+  c = fanin(boring("pilathraj"),boring("ragu"))
+  
+  for i:=0; i<10; i++{
+    fmt.Println(<-c)    
+  }
+}
