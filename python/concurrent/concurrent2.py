08a947a0ff8dd70253571c7afa725e55305f8e9f
pilathraj <pilathraj@gmail.com>
2023-06-15 19:07:14 +0530

    Create concurrent2.py

---
+++ python/concurrent/concurrent2.py
@@ -0,0 +1,25 @@
+import concurrent.futures
+import time
+
+t1 = None
+def get_a(a):
+    time.sleep(1)
+    return a
+def get_b():
+    time.sleep(1)
+    print("get B")
+    return 100
+
+def call():
+    global t1
+    with concurrent.futures.ThreadPoolExecutor() as my_executor:
+        t1 = my_executor.submit(get_b)
+start = time.perf_counter()
+call()
+time.sleep(2)
+print(get_a(20)+ t1.result())
+finish = time.perf_counter()
+print(f"total time {finish-start}") 
+
+
+
