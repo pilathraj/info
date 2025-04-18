1ce53d0985e8faa7206b92ee493e53a0ee390746
pilathraj <pilathraj@gmail.com>
2025-04-18 21:01:43 +0530

    Update SimpleStorage.sol

--- blockchain/ch2/SimpleStorage.sol
+++ blockchain/ch2/SimpleStorage.sol
@@ -5,6 +5,15 @@ contract SimpleStorage{
     // this will initialized to zero
     uint256 public faviouriteNumber;
 
+    struct People {
+        string name;
+        uint256 faviouriteNumber;
+    }
+    People[] public people;
+    mapping(string=>uint256) public nameToFaviouriateNumber;
+
+    People public person = People({name:"hello", faviouriteNumber:0});
+
     function storeFaviouriteNumber(uint256 _faviouriteNumber) public  {
         faviouriteNumber = _faviouriteNumber;
     }
@@ -15,6 +24,11 @@ contract SimpleStorage{
     // do some mathematical without change the state and return anything - pure function
     function retrive2(uint256 _faviouriteNumber) public pure {
         _faviouriteNumber+_faviouriteNumber;
+    }
+
+    function addPersion(string memory _name, uint256 _faviouriteNumber) public {
+        people.push(People({name:_name, faviouriteNumber:_faviouriteNumber}));
+        nameToFaviouriateNumber[_name]=_faviouriteNumber;
     }
 
 }
