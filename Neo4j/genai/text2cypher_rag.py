fe8e3e8aa58d37cc97bafd347f7f39b064385ca9
pilathraj <pilathraj@gmail.com>
2025-07-24 18:45:35 +0530

    Update text2cypher_rag.py

--- Neo4j/genai/text2cypher_rag.py
+++ Neo4j/genai/text2cypher_rag.py
@@ -22,11 +22,43 @@ t2c_llm = OpenAILLM(
     model_params={"temperature": 0}
 )
 
+# Cypher examples as input/query pairs
+examples = [
+    "USER INPUT: 'Get user ratings for a movie?' QUERY: MATCH (u:User)-[r:RATED]->(m:Movie) WHERE m.title = 'Movie Title' RETURN r.rating"
+]
+
+# Specify your own Neo4j schema
+neo4j_schema = """
+Node properties:
+Person {name: STRING, born: INTEGER}
+Movie {tagline: STRING, title: STRING, released: INTEGER}
+Genre {name: STRING}
+User {name: STRING}
+
+Relationship properties:
+ACTED_IN {role: STRING}
+RATED {rating: INTEGER}
+
+The relationships:
+(:Person)-[:ACTED_IN]->(:Movie)
+(:Person)-[:DIRECTED]->(:Movie)
+(:User)-[:RATED]->(:Movie)
+(:Movie)-[:IN_GENRE]->(:Genre)
+"""
+
 # Build the retriever
 retriever = Text2CypherRetriever(
     driver=driver,
     llm=t2c_llm,
+    examples=examples
 )
+# Pass custom schema if needed else Text2CypherRetriever will retrive schema from Neo4j db
+# retriever = Text2CypherRetriever(
+#     driver=driver,
+#     llm=t2c_llm,
+#     examples=examples,  
+#     neo4j_schema=neo4j_schema
+# )
 
 llm = OpenAILLM(model_name="gpt-4o-mini")
 rag = GraphRAG(retriever=retriever, llm=llm)
@@ -34,7 +66,7 @@ rag = GraphRAG(retriever=retriever, llm=llm)
 query_text = "Which movies did Hugo Weaving acted in?"
 query_text = "What are examples of Action movies?"
 query_text = "What year was the movie Babe released?" # ans: The movie Babe was released in 1995.
-
+query_text = "What user gives the lowest ratings?"
 
 response = rag.search(
     query_text=query_text,
